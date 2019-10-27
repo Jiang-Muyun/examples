698f59431d53d7d0888fac3af103140df2ebf3c7
James <jmy9605@gmail.com>
2019-10-28 00:06:52 +0800

    mod

--- time_sequence_prediction/train.py
+++ time_sequence_prediction/train.py
@@ -9,49 +9,50 @@ import matplotlib.pyplot as plt
 
 
 device = 'cuda'
-class Sequence(nn.Module):
-    def __init__(self):
-        super(Sequence, self).__init__()
-        self.lstm1 = nn.LSTMCell(1, 51)
-        self.lstm2 = nn.LSTMCell(51, 51)
-        self.linear = nn.Linear(51, 1)
+# class Sequence(nn.Module):
+#     def __init__(self):
+#         super(Sequence, self).__init__()
+#         self.lstm1 = nn.LSTMCell(1, 51)
+#         self.lstm2 = nn.LSTMCell(51, 51)
+#         self.linear = nn.Linear(51, 1)
 
-    def forward(self, input, future = 0):
-        outputs = []
-        print(input.shape)
-        exit()
-        h_t = torch.zeros(input.size(0), 51, dtype=torch.double).to(device)
-        c_t = torch.zeros(input.size(0), 51, dtype=torch.double).to(device)
-        h_t2 = torch.zeros(input.size(0), 51, dtype=torch.double).to(device)
-        c_t2 = torch.zeros(input.size(0), 51, dtype=torch.double).to(device)
+#     def forward(self, input, future = 0):
+#         # input shape -> (97, 999)
+#         outputs = []
+#         h_t = torch.zeros(input.size(0), 51, dtype=torch.double).to(device)
+#         c_t = torch.zeros(input.size(0), 51, dtype=torch.double).to(device)
+#         h_t2 = torch.zeros(input.size(0), 51, dtype=torch.double).to(device)
+#         c_t2 = torch.zeros(input.size(0), 51, dtype=torch.double).to(device)
 
-        for i, input_t in enumerate(input.chunk(input.size(1), dim=1)):
-            h_t, c_t = self.lstm1(input_t, (h_t, c_t))
-            h_t2, c_t2 = self.lstm2(h_t, (h_t2, c_t2))
-            output = self.linear(h_t2)
-            outputs += [output]
+#         for i, input_t in enumerate(input.chunk(input.size(1), dim=1)):
+#             h_t, c_t = self.lstm1(input_t, (h_t, c_t))
+#             h_t2, c_t2 = self.lstm2(h_t, (h_t2, c_t2))
+#             output = self.linear(h_t2)
+#             outputs += [output]
+
+#         for i in range(future):# if we should predict the future
+#             h_t, c_t = self.lstm1(output, (h_t, c_t))
+#             h_t2, c_t2 = self.lstm2(h_t, (h_t2, c_t2))
+#             output = self.linear(h_t2)
+#             outputs += [output]
         
-        for i in range(future):# if we should predict the future
-            h_t, c_t = self.lstm1(output, (h_t, c_t))
-            h_t2, c_t2 = self.lstm2(h_t, (h_t2, c_t2))
-            output = self.linear(h_t2)
-            outputs += [output]
-        
-        outputs = torch.stack(outputs, 1).squeeze(2)
-        return outputs
-
+#         outputs = torch.stack(outputs, 1).squeeze(2)
+#         return outputs
 
 if __name__ == '__main__':
-    # set random seed to 0
     np.random.seed(0)
     torch.manual_seed(0)
-    # load data and make training set
-    data = torch.load('traindata.pt')
+
+    data = torch.load('traindata.pt') #(100, 1000)
+
+    # 97, 999
     input = torch.from_numpy(data[3:, :-1]).to(device)
     target = torch.from_numpy(data[3:, 1:]).to(device)
+
+    # 3, 999
     test_input = torch.from_numpy(data[:3, :-1]).to(device)
     test_target = torch.from_numpy(data[:3, 1:]).to(device)
-    # build the model
+
     seq = Sequence()
     seq.double()
     seq.to(device)
