91aef59faae51e56b804b57386ad0d768ef90d14
James Murray <jmy9605@gmail.com>
2019-10-29 16:59:50 +0800

    mod

--- word_language_model/main.py
+++ word_language_model/main.py
@@ -11,43 +11,25 @@ import data
 import model
 
 parser = argparse.ArgumentParser(description='PyTorch Wikitext-2 RNN/LSTM Language Model')
-parser.add_argument('--data', type=str, default='./data/wikitext-2',
-                    help='location of the data corpus')
-parser.add_argument('--model', type=str, default='LSTM',
-                    help='type of recurrent net (RNN_TANH, RNN_RELU, LSTM, GRU, Transformer)')
-parser.add_argument('--emsize', type=int, default=200,
-                    help='size of word embeddings')
-parser.add_argument('--nhid', type=int, default=200,
-                    help='number of hidden units per layer')
-parser.add_argument('--nlayers', type=int, default=2,
-                    help='number of layers')
-parser.add_argument('--lr', type=float, default=20,
-                    help='initial learning rate')
-parser.add_argument('--clip', type=float, default=0.25,
-                    help='gradient clipping')
-parser.add_argument('--epochs', type=int, default=40,
-                    help='upper epoch limit')
-parser.add_argument('--batch_size', type=int, default=20, metavar='N',
-                    help='batch size')
-parser.add_argument('--bptt', type=int, default=35,
-                    help='sequence length')
-parser.add_argument('--dropout', type=float, default=0.2,
-                    help='dropout applied to layers (0 = no dropout)')
-parser.add_argument('--tied', action='store_true',
-                    help='tie the word embedding and softmax weights')
-parser.add_argument('--seed', type=int, default=1111,
-                    help='random seed')
-parser.add_argument('--cuda', action='store_true',
-                    help='use CUDA')
-parser.add_argument('--log-interval', type=int, default=200, metavar='N',
-                    help='report interval')
-parser.add_argument('--save', type=str, default='model.pt',
-                    help='path to save the final model')
-parser.add_argument('--onnx-export', type=str, default='',
-                    help='path to export the final model in onnx format')
+parser.add_argument('--data', type=str, default='./data/wikitext-2',help='location of the data corpus')
+parser.add_argument('--model', type=str, default='LSTM',help='type of recurrent net (RNN_TANH, RNN_RELU, LSTM, GRU, Transformer)')
+parser.add_argument('--emsize', type=int, default=200,help='size of word embeddings')
+parser.add_argument('--nhid', type=int, default=200,help='number of hidden units per layer')
+parser.add_argument('--nlayers', type=int, default=2,help='number of layers')
+parser.add_argument('--lr', type=float, default=20,help='initial learning rate')
+parser.add_argument('--clip', type=float, default=0.25,help='gradient clipping')
+parser.add_argument('--epochs', type=int, default=40,help='upper epoch limit')
+parser.add_argument('--batch_size', type=int, default=20, metavar='N',help='batch size')
+parser.add_argument('--bptt', type=int, default=35,help='sequence length')
+parser.add_argument('--dropout', type=float, default=0.2,help='dropout applied to layers (0 = no dropout)')
+parser.add_argument('--tied', action='store_true',help='tie the word embedding and softmax weights')
+parser.add_argument('--seed', type=int, default=1111,help='random seed')
+parser.add_argument('--cuda', action='store_true',help='use CUDA')
+parser.add_argument('--log-interval', type=int, default=200, metavar='N',help='report interval')
+parser.add_argument('--save', type=str, default='model.pt',help='path to save the final model')
+parser.add_argument('--onnx-export', type=str, default='',help='path to export the final model in onnx format')
 
-parser.add_argument('--nhead', type=int, default=2,
-                    help='the number of heads in the encoder/decoder of the transformer model')
+parser.add_argument('--nhead', type=int, default=2,help='the number of heads in the encoder/decoder of the transformer model')
 
 args = parser.parse_args()
 
@@ -184,8 +166,7 @@ def train():
         if batch % args.log_interval == 0 and batch > 0:
             cur_loss = total_loss / args.log_interval
             elapsed = time.time() - start_time
-            print('| epoch {:3d} | {:5d}/{:5d} batches | lr {:02.2f} | ms/batch {:5.2f} | '
-                    'loss {:5.2f} | ppl {:8.2f}'.format(
+            print('| epoch {:3d} | {:5d}/{:5d} batches | lr {:02.2f} | ms/batch {:5.2f} | ''loss {:5.2f} | ppl {:8.2f}'.format(
                 epoch, batch, len(train_data) // args.bptt, lr,
                 elapsed * 1000 / args.log_interval, cur_loss, math.exp(cur_loss)))
             total_loss = 0
@@ -213,8 +194,7 @@ try:
         val_loss = evaluate(val_data)
         print('-' * 89)
         print('| end of epoch {:3d} | time: {:5.2f}s | valid loss {:5.2f} | '
-                'valid ppl {:8.2f}'.format(epoch, (time.time() - epoch_start_time),
-                                           val_loss, math.exp(val_loss)))
+                'valid ppl {:8.2f}'.format(epoch, (time.time() - epoch_start_time),                       val_loss, math.exp(val_loss)))
         print('-' * 89)
         # Save the model if the validation loss is the best we've seen so far.
         if not best_val_loss or val_loss < best_val_loss:
